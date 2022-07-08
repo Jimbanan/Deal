9a5f6d8584ae9e7f1a4a0927254c8f14b9c05473
Jimbanan <uservice371@mail.ru>
2022-07-08 18:39:32 +0300

    Add Interface

---
+++ src/main/java/com/neoflex/deal/services/KafkaProducerServiceImpl.java
@@ -0,0 +1,35 @@
+package com.neoflex.deal.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.neoflex.deal.dto.EmailMessage;
+import com.neoflex.deal.enums.Theme;
+import com.neoflex.deal.models.Application;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducerServiceImpl implements KafkaProducerService{
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+    private final ApplicationService applicationService;
+
+    public void send(String topic, Theme theme, Long id) throws JsonProcessingException {
+        Application app = applicationService.getApplication(id);
+
+        EmailMessage emailMessage = EmailMessage.builder()
+                .address(app.getClient().getEmail())
+                .theme(theme)
+                .applicationId(app.getId())
+                .build();
+
+        String json = objectMapper.writeValueAsString(emailMessage);
+
+        kafkaTemplate.send(topic, json);
+    }
+}
